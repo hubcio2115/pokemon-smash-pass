1cef5ec7fd4962a77aca76ead0b2bdc9a8491564
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-02-04 12:48:39 +0100

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
-      config: { path: './tailwind.config.cjs' },
+      config: {
+        path: './tailwind.config.cjs',
+      },
     }),
+    preact(),
   ],
 });
