8134557cb8acf571eb10fd5f19c19e2a4daa2f94
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-02-05 16:33:12 +0100

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import preact from '@astrojs/preact';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind({
       config: {
